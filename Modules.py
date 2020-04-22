1f9edd2bc67134f5bb11f11f1c96a40f6a7d851a
Heejo You <codejin2@gmail.com>
2020-04-22 22:14:49 +0900

    Initial update

--- Modules.py
+++ Modules.py
@@ -9,7 +9,7 @@ class Encoder(tf.keras.layers.Layer):
         self.layer = tf.keras.Sequential()
         self.layer.add(tf.keras.layers.Dense(
             units= hp_Dict['Encoder']['Hidden1']['Size'],
-            activation= 'elu'
+            activation= 'relu'
             ))
         self.layer.add(tf.keras.layers.Dropout(
             rate= hp_Dict['Encoder']['Hidden1']['Dropout_Rate']
@@ -27,15 +27,15 @@ class Encoder(tf.keras.layers.Layer):
 
     def call(self, inputs):
         new_Tensor = self.layer(inputs)
-        mean, std = tf.split(new_Tensor, num_or_size_splits= 2, axis= -1)
-        std = tf.nn.softplus(std)
+        mean, std = tf.split(new_Tensor, num_or_size_splits= 2, axis= -1)        
 
         return mean, std
 
 class Latent(tf.keras.layers.Layer):
     def call(self, inputs):
         mean, std = inputs
-        return mean + std * tf.random.normal(tf.shape(mean), 0, 1, dtype=mean.dtype)
+        std = tf.nn.softplus(std)
+        return mean + std * tf.random.normal(tf.shape(std), 0, 1, dtype=mean.dtype)
 
 class Decoder(tf.keras.layers.Layer):
     def build(self, input_shapes):
@@ -49,7 +49,7 @@ class Decoder(tf.keras.layers.Layer):
             ))
         self.layer.add(tf.keras.layers.Dense(
             units= hp_Dict['Decoder']['Hidden2']['Size'],
-            activation= 'elu'
+            activation= 'relu'
             ))
         self.layer.add(tf.keras.layers.Dropout(
             rate= hp_Dict['Decoder']['Hidden2']['Dropout_Rate']
@@ -69,14 +69,10 @@ class Loss(tf.keras.layers.Layer):
         marginal_Likelihood = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(
             labels= labels,
             logits= logits
-            ), axis= -1)
-        kl_Divergence = 0.5 * tf.reduce_sum(
-            encoder_Mean ** 2 + encoder_Std ** 2 - tf.math.log(encoder_Std ** 2 + 1e-8),
-            axis= -1
+            ))
+        kl_Divergence = -0.5 * tf.reduce_sum(
+            1 + encoder_Std - encoder_Mean ** 2 - tf.exp(encoder_Std)
             )
-        
-        marginal_Likelihood = tf.reduce_mean(marginal_Likelihood)
-        kl_Divergence = tf.reduce_mean(kl_Divergence)
 
         elbo_Loss = marginal_Likelihood + kl_Divergence
 
